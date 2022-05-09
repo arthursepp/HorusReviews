36fd9012c1fb169e643a30e5e7a7a7ae096ffae2
Arthur <arthursouzasepp@gmail.com>
2022-05-09 19:48:53 -0300

    Android em Kotlin, documentos novos, Banco de Dados.

---
+++ Codigo/MVC_CSHTML/HorusReviews_MVC_13_04_22/HorusReviews_MVC_13_04_22/HorusReviews_MVC_13_04_22/HorusReviews_MVC_12_04_22/JS/script.js
@@ -0,0 +1,5 @@
+﻿function msgConfirm() {
+
+    alert("Usuário cadastrado!");
+
+}
---
+++ Codigo/MVC_CSHTML/HorusReviews_MVC_13_04_22/HorusReviews_MVC_13_04_22/HorusReviews_MVC_13_04_22/HorusReviews_MVC_12_04_22/Models/DataModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HorusReviews_MVC_12_04_22.Models
+{
+    public class DataModel
+    {
+
+        public int CodUser { get; set; }
+        public string UserName { get; set; }
+        public string Nome { get; set; }
+        public System.DateTime DataNasc { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
